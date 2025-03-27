39ef6d28a3e4da6b70d8d23ff08a3665a8be0fa4
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-03-27 15:37:07 +0100

    removed landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import LandingPage from "./landingPage";
+import Auth from "./auth";
+
 
 export default function Home() {
   return (
     <div>
-      <LandingPage />
+      <Auth />
     </div>
   );
 }
